80c0baf90ba7b845ec4f90e72912708bc84b822f
xwq <2042616571@qq.com>
2023-07-10 17:54:46 +0800

    feature：登录接口编写

--- study-project-backend/src/main/java/com/example/StudyProjectBackendApplication.java
+++ study-project-backend/src/main/java/com/example/StudyProjectBackendApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class StudyProjectBackendApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(StudyProjectBackendApplication.class, args);
     }
 
